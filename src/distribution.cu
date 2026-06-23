5467521673a17e72c36139ffd4c99b82a9e6f75c
gmtiddia <gianmarcotiddia@gmail.com>
2026-06-23 16:00:29 +0100

    test distrib

--- src/distribution.cu
+++ src/distribution.cu
@@ -174,8 +174,8 @@ Distribution::getArray( curandGenerator_t& gen, int64_t n_elem, int i_vect )
     float low  = expf(log_low);
     float high = expf(log_high);
 
-    float mu_nd = expf(mu_[ i_vect ] + 0.5*sigma_[ i_vect ]*sigma_[ i_vect ])
-    float std_nd = (expf(sigma_[ i_vect ]*sigma_[ i_vect ]) - 1) * expf(2*mu_[ i_vect ] + sigma_[ i_vect ]*sigma_[ i_vect ])
+    float mu_nd = expf(mu_[ i_vect ] + 0.5*sigma_[ i_vect ]*sigma_[ i_vect ]);
+    float std_nd = (expf(sigma_[ i_vect ]*sigma_[ i_vect ]) - 1) * expf(2*mu_[ i_vect ] + sigma_[ i_vect ]*sigma_[ i_vect ]);
 
     CURAND_CALL( curandGenerateLogNormal( gen, d_array_pt_, n_elem,  mu_nd, std_nd) );
     //CURAND_CALL( curandGenerateUniform( gen, d_array_pt_, n_elem ) );
