0480ed6b45d2a1944a6e36f009a7be6672f22a70
Gianmarco Tiddia <gtiddia0@lrdn2279.leonardo.local>
2026-06-25 10:36:55 +0200

    remove tab from warning message

--- src/poiss_gen.cu
+++ src/poiss_gen.cu
@@ -135,7 +135,7 @@ poiss_gen::Calibrate( double, float )
   // if the device is not connected to any other node, the calibration continues without rising errors
   if ( n_dir_conn_ == 0 )
   {
-    std::cout << "\tWarning: Poisson generator devices created but not used.\n";
+    std::cout << "Warning: Poisson generator devices created but not used.\n";
     return 0; 
   }
 
